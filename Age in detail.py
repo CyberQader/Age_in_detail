dfdc415646e311c6c7b01726c2b330392f684996
unknown <youremail@example.com>
2026-03-22 15:05:50 +0200

    Initial commit

---
+++ Age in detail.py
@@ -0,0 +1,12 @@
+age = int(input ('Enter Your Age : '))
+months = age * 12
+weeks = months * 4
+days = weeks * 365
+hours = days * 24
+minutes = hours * 60
+print(f' Your Lived For : ')
+print(f'{months} >>>>> months')
+print(f'{weeks} >>>>> weeks')
+print(f'{days} >>>>> days')
+print(f'{hours} >>>>> hours')
+print(f'{minutes} >>>>> minutes')
